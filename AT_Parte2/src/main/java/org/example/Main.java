b8dfc6c9622aa8ed20c73f5c840fa6ea8adcf210
delunacomunicacao@gmail.com <delunacomunicacao@gmail.com>
2023-12-04 13:55:39 -0300

    verificação ACK ou NACK

--- AT_Parte2/src/main/java/org/example/Main.java
+++ AT_Parte2/src/main/java/org/example/Main.java
@@ -1,35 +1,41 @@
 package org.example;
 
+
 import javax.swing.*;
 import java.awt.*;
 
+import static org.example.Methods.*;
+
 public class Main {
+    static String resposta = "";
+    static String codigoCartaostr = "";
+    static String codigoCartaoValido = "12345678";
     public static void main(String[] args) {
         JFrame janela = new JFrame("Exercício 2");
         janela.setSize(500,150);
         janela.setResizable(false);
 
-        JPanel painel = new JPanel(new GridLayout(2,1));
+        JPanel painel = new JPanel();
 
         //Painel pro input do usuário
-        JPanel userInputPanel = new JPanel(new FlowLayout());
+        JLabel codCartaoLabel = new JLabel("Código do cartão: ");
+        codCartaoLabel.setFont(new Font("Arial", Font.PLAIN, 16));
         JTextField tf = new JTextField(20);
-        JButton btnOk = new JButton("ok");
-        btnOk.setPreferredSize(new Dimension(50, 20));
-        userInputPanel.add(tf);
-        userInputPanel.add(btnOk);
-        //Mensagem de retorno
-        JPanel retornoPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        JLabel retorno = new JLabel("Retorno: Nenhum");
-        retorno.setFont(new Font("Arial", Font.BOLD, 16));
-        retornoPanel.add(retorno);
 
-        painel.add(userInputPanel);
-        painel.add(retornoPanel);
+        //Painel principal
+        painel.add(codCartaoLabel);
+        painel.add(tf);
         janela.add(painel);
+
         janela.setLocationRelativeTo(null);
         janela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        janela.setLayout(new FlowLayout());
         janela.setVisible(true);
 
+        //Verificação ciclica do valor da resposta
+        while (true) {
+            verificarValorResposta(tf, janela, painel);
+        }
     }
+
 }
